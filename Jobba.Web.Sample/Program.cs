8c2e530bb272be319e1e6d5e000eb4c218885a1f
Jacob Atchley <yaaqob.atchley@gmail.com>
2021-11-30 08:44:47 -0600

    upgrade

--- Jobba.Web.Sample/Program.cs
+++ Jobba.Web.Sample/Program.cs
@@ -1,29 +1,20 @@
 using Jobba.Store.Mongo;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+using Jobba.Web.Sample;
+using Microsoft.AspNetCore.Builder;
 using MongoDB.Bson.Serialization;
 
-namespace Jobba.Web.Sample
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            JobbaMongoDbConfigurator.Configure();
+var builder = WebApplication.CreateBuilder(args);
+JobbaMongoDbConfigurator.Configure();
 
-            BsonClassMap.RegisterClassMap<SampleWebJobParameters>();
-            BsonClassMap.RegisterClassMap<SampleWebJobState>();
-            BsonClassMap.RegisterClassMap<SampleFaultWebJobParameters>();
-            BsonClassMap.RegisterClassMap<SampleFaultWebJobState>();
+BsonClassMap.RegisterClassMap<SampleWebJobParameters>();
+BsonClassMap.RegisterClassMap<SampleWebJobState>();
+BsonClassMap.RegisterClassMap<SampleFaultWebJobParameters>();
+BsonClassMap.RegisterClassMap<SampleFaultWebJobState>();
 
-            CreateHostBuilder(args).Build().Run();
-        }
+var startup = new Startup(builder.Configuration);
+startup.ConfigureServices(builder.Services);
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
-}
+var app = builder.Build();
+startup.Configure(app, app.Environment);
+
+await app.RunAsync();
