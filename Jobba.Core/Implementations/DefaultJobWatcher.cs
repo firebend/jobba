27ec1d11a4f056e8c66e9116b4964ebfaa746849
Terry Schneider <tschneider@firebend.com>
2026-06-03 13:54:04 -0500

    refactor: remove outdated comments and improve clarity in DefaultJobWatcher

--- Jobba.Core/Implementations/DefaultJobWatcher.cs
+++ Jobba.Core/Implementations/DefaultJobWatcher.cs
@@ -7,10 +7,6 @@ using Jobba.Core.Models;
 
 namespace Jobba.Core.Implementations;
 
-//notes: this class is better if its a generic so we can start the request of the job and pull back the parameters and states as typed objects
-// if we register this watcher when we add a job, we can resolve it from the IoC container in our JobWatchEventSubscriber
-// in the subscriber the event will have the types as strings
-// we can create a type and resolve it form the IoC as this interface and call watch job async using reflection :D
 public class DefaultJobWatcher<TJob, TJobParams, TJobState> : IJobWatcher<TJob, TJobParams, TJobState>
     where TJob : IJob<TJobParams, TJobState>
     where TJobParams : IJobParams
